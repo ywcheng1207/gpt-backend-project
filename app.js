30c8a72227d16ea22453946ffe9c141e4dfbac90
ywcheng1207 <ywcheng1207.git@gmail.com>
2023-10-31 22:17:56 +0800

    Update library

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
 // 套件
 const express = require('express')
-const bodyParser = require('body-parser')
+// const bodyParser = require('body-parser')
 const routes = require('./routes')
 require('dotenv').config()
 const PORT = process.env.PORT || 8080
+const cors = require('cors')
 
 //
 require('./config/mongoose')
 const app = express()
 
 // use
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
+// app.use(bodyParser.urlencoded({ extended: true }))
+// app.use(bodyParser.json())
+app.use(express.json())
+app.use(cors())
 app.use(routes)
 
 // listen
